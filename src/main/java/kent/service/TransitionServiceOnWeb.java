8d8022efb5566f3d6b348edc16c41e0aa35e3d74
kent.Zhang <361915607@qq.com>
2017-10-25 14:55:53 +0800

    lambda support

--- src/main/java/kent/service/TransitionServiceOnWeb.java
+++ src/main/java/kent/service/TransitionServiceOnWeb.java
@@ -2,7 +2,6 @@ package kent.service;
 
 import kent.model.JsonResult;
 import kent.model.SuccessResult;
-import org.apache.log4j.Logger;
 import org.dom4j.Document;
 import org.dom4j.DocumentException;
 import org.dom4j.Element;
@@ -17,9 +16,6 @@ import java.util.*;
 
 @Service
 public class TransitionServiceOnWeb {
-
-    //日志记录
-    protected Logger logger = Logger.getLogger(TransitionServiceOnWeb.class);
 
     //公理生成部分的变量列表
     private List<String> varList = new ArrayList<>();
@@ -59,7 +55,6 @@ public class TransitionServiceOnWeb {
     /**
      * 类型转换部分
      */
-
     //复合类型转换
     private StringBuilder getComplexType(Element root){
         StringBuilder typeBuilder = new StringBuilder();
@@ -67,36 +62,30 @@ public class TransitionServiceOnWeb {
         if(list.size() == 0){
             root.addNamespace("xs","http://www.w3.org/2001/XMLSchema");
             list = root.selectNodes("//wsdl:types//xs:complexType");
-
         }
-        for (Object aList : list) {
-
-            Element element = (Element) aList;
-
+        list.forEach(l->{
+            Element element = (Element) l;
             // get the record type
             String complexTypeName = element.attributeValue("name");
             typeBuilder.append("type ").append(complexTypeName).append("=record").append("<br/>");
-
             //get the normal type
             Iterator<?> sequenceIterator = element.nodeIterator();
-            while (sequenceIterator.hasNext()) {
-                Node sequenceNode = (Node) sequenceIterator.next();
-                if (sequenceNode instanceof Element) {
-                    Element sequenceElement = (Element) sequenceNode;
+            sequenceIterator.forEachRemaining(s->{
+                if (s instanceof Element) {
+                    Element sequenceElement = (Element) s;
                     Iterator<?> eleIterator = sequenceElement.nodeIterator();
-                    while (eleIterator.hasNext()) {
-                        Node eleNode = (Node) eleIterator.next();
-                        if (eleNode instanceof Element) {
-                            Element ele = (Element) eleNode;
-                            String normalName = ele.attributeValue("name");
-                            String normalType = ele.attributeValue("type");
+                    eleIterator.forEachRemaining(ele->{
+                        if(ele instanceof Element){
+                            Element elem = (Element) ele;
+                            String normalName = elem.attributeValue("name");
+                            String normalType = elem.attributeValue("type");
                             String afterNormalType = spiltPrefix(normalType);
                             typeBuilder.append(normalName).append(": ").append(afterNormalType).append(";").append("<br/>");
                         }
-                    }
+                    });
                 }
-            }
-        }
+            });
+        });
         typeBuilder.append("end").append("<br/>").append("<br/>");
         return typeBuilder;
     }
@@ -112,35 +101,36 @@ public class TransitionServiceOnWeb {
         String specName = root.element("service").attributeValue("name");
         specMain.append(specName).append("<br/>");
 
-
         //类型转换构建imports列表
         //set complexTypeName to imports list
         specMain.append("imports: ");
-        List list = root.selectNodes("//types//complexType");
+        List<?> list = root.selectNodes("//types//complexType");
+        //前缀补充
         if(list.size() == 0){
             root.addNamespace("xs","http://www.w3.org/2001/XMLSchema");
             list = root.selectNodes("//wsdl:types//xs:complexType");
 
         }
-        int i = 1;
+
         varList.add("message");
-        for (Object aList : list){
-            Element element = (Element) aList;
+        list.forEach(l->{
+            int index = 0;
+            Element element = (Element) l;
             String complexTypeName = element.attributeValue("name");
             specMain.append(complexTypeName).append(",");
             //复杂类型添加到变量列表中
-            varList.add(i++,complexTypeName);
-        }
+            varList.add(++index,complexTypeName);
+        });
         //set messageName to imports list
-        List list1 =  root.selectNodes("//message");
+        List<?> list1 =  root.selectNodes("//message");
         if(list1.size() == 0) list1 = root.selectNodes("//wsdl:message");
-        for (Object bList : list1){
-            Element element = (Element)bList;
+        list1.forEach(l->{
+            Element element = (Element) l;
             String messageName = element.attributeValue("name");
             //拿到import列表 防止重复添加
-            if(Arrays.asList(specMain.toString().split(":")[1].substring(1).split(",")).contains(messageName)) continue;
+            if(!(Arrays.asList(specMain.toString().split(":")[1].substring(1).split(",")).contains(messageName)))
             specMain.append(messageName).append(",");
-        }
+        });
         //delete the last ,
         specMain.deleteCharAt(specMain.length()-1).append("<br/>").append("ops:").append("<br/>");
 
@@ -211,9 +201,9 @@ public class TransitionServiceOnWeb {
                                     //提取操作名 getMaxMinTemps
                                     String operationName = operationElement.attributeValue("name");
                                     ob.append(operationName).append(": ");
-                                    Iterator<Node> inOutIt = operationElement.nodeIterator();
+                                    Iterator<?> inOutIt = operationElement.nodeIterator();
                                     while (inOutIt.hasNext()){
-                                        Node inOutNode  =  inOutIt.next();
+                                        Node inOutNode  =  (Node) inOutIt.next();
                                         if(inOutNode instanceof Element){
                                             Element inOutElement = (Element) inOutNode;
                                             String inOutName = inOutElement.getName();
@@ -273,48 +263,39 @@ public class TransitionServiceOnWeb {
         StringBuilder axiomsMain = new StringBuilder();
         axioms.append("<br/>").append("axioms: ").append("<br/>");
         //处理变量列表中的元素，取一半长度的字符串
-        String temp;
+
         varString.append("var ");
         // get var list
-        for(String varl : varList){
-            temp = varl.substring(0,varl.length()/2+1);
-            temp = temp+": "+varl+"; ";
+        varList.forEach(v->{
+            String temp;
+            temp = v.substring(0,v.length()/2+1);
+            temp = temp+": "+v+"; ";
             varString.append(temp);
-        }
-
+        });
 
         //生成公理说明主体部分
         String subPortTypeName = portTypeName.substring(0,portTypeName.length()/2+1);
-        //用于保存临时key\value值
-        String keyNext;
-        String valueNext;
         //{PlaceAndDate=pdrec, MaxMinTemp=mmtre, InDataFault=errmes} dataAndType Map序列
         Set<String> complexTypeSet = dataAndType.keySet();
         Iterator<String> keySetIt = complexTypeSet.iterator();
         //进行双重循环，围绕每一个key值进行迭代，构造公理
-        while (keySetIt.hasNext()) {
-            Iterator<String> tempIt = complexTypeSet.iterator();
-            keyNext =  keySetIt.next();
-            valueNext = dataAndType.get(keyNext);
-            while (tempIt.hasNext()) {
-                String tempKey = tempIt.next();
-                String subP = subPortTypeName + ".set_ " + keyNext + " (" + this.spiltPrefix(valueNext) + ").get_ ";
+        keySetIt.forEachRemaining(k->{
+            Iterator<String> it = complexTypeSet.iterator();
+            it.forEachRemaining(i -> {
+                String subP = subPortTypeName + ".set_ " + k + " (" + this.spiltPrefix(dataAndType.get(k)) + ").get_ ";
                 axiomsMain.append(subP);
-                if(tempKey.equals(keyNext)) {
-                    String kvN = keyNext + " ="  + this.spiltPrefix(valueNext) + "<br/>";
+                if (i.equals(k)) {
+                    String kvN = k + " =" + this.spiltPrefix(dataAndType.get(k)) + "<br/>";
                     axiomsMain.append(kvN);
-                }else {
-                    String tempK = tempKey+" = "+subPortTypeName+".get_ "+tempKey+"<br/>";
+                } else {
+                    String tempK = i + " = " + subPortTypeName + ".get_ " + i + "<br/>";
                     axiomsMain.append(tempK);
                 }
-            }
-        }
+            });
+        });
         axioms.append(varString).append("<br/>");
         axioms.append(axiomsMain);
         axioms.append("end-spec");
         return axioms;
     }
-
-
-
 }
